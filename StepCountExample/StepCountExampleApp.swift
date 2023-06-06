3c350b20ea069fb31f6bec648f018adc100b7594
Bryan Langslet <blangslet@gmail.com>
2023-06-06 05:11:56 -0500

    Initial Commit

---
+++ StepCountExample/StepCountExampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  StepCountExampleApp.swift
+//  StepCountExample
+//
+//  Created by Bryan Langslet on 6/6/23.
+//
+
+import SwiftUI
+
+@main
+struct StepCountExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
